d8412f27da074f98e195e2bff2a7293b4b639c57
Chomtana <chomtana001@gmail.com>
2019-04-07 11:31:31 +0700

    update for github page

---
+++ docs/precache-manifest.feea23df51ab483685cac98b4ca135ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "45905ce12b4739c794a5ebdaee506202",
+    "url": "/static/media/THSarabunNew.45905ce1.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab5f91b21ed0a0f79368",
+    "url": "/static/js/main.2b3a0a2c.chunk.js"
+  },
+  {
+    "revision": "c0d97e76f580c7111212",
+    "url": "/static/js/2.607afdf4.chunk.js"
+  },
+  {
+    "revision": "ab5f91b21ed0a0f79368",
+    "url": "/static/css/main.932dae64.chunk.css"
+  },
+  {
+    "revision": "3f6582462dd48d32a52d1831ae1aad34",
+    "url": "/index.html"
+  }
+];
